70a044d7ea0bb19a7185e194eac3e79244716a5f
Samuel Velasco <samuelvelasco2698@gmail.com>
2022-07-25 16:54:04 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref, computed, onMounted } from "vue";
+
+const query = ref("");
+const my_amine = ref([]);
+const search_results = ref([]);
+
+const my_anime_asc = computed(() => {
+  return my_anime.value.sort((a, b) => {
+    return a.title.localeCompare(b.title);
+  });
+});
+
+// const searchAnime = () => {
+//   const url = `https://api.jikan.moe/v4/anime?q=${query.value}`;
+//   fetch(url).then((res) => res.json);
+// };
+</script>
+
+<template>
+  <main>Hello World :)</main>
+</template>
+
+<style></style>
